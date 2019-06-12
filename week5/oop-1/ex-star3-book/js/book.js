535f6ac89b23f659d9d97d81b1c4373a4a394a9b
Liora <lioralehrer@gmail.com>
2019-06-12 17:50:50 +0300

    console.log is off

--- week5/oop-1/ex-star3-book/js/book.js
+++ week5/oop-1/ex-star3-book/js/book.js
@@ -1,33 +1,32 @@
 // Create a book object, where each object describes a book and has properties for the title, the author and
 // whether or not you have already read this book
-class Book{
-    constructor(title , author , beenRead){
+class Book {
+    constructor(title, author, beenRead) {
         this.title = title;
         this.author = author;
         this.beenRead = beenRead;
     }
 }
 //Create a few instances of this object
-let alice = new Book ("alice", "karl Luise", true);
-let paul = new Book ("paul street", "nemechek", true);
-let makeIt = new Book ("make it rain", "dont know", false);
-let hobit = new Book ("the hobit", "J.R.R. Tolkin", false);
+let alice = new Book("alice", "karl Luise", true);
+let paul = new Book("paul street", "nemechek", true);
+let makeIt = new Book("make it rain", "dont know", false);
+let hobit = new Book("the hobit", "J.R.R. Tolkin", false);
 // Create a global method that logs the book title and book author like so: "The Hobbit by J.R.R. Tolkien".
-function logTitleAndAuthor(book){
-   console.log(`${book.title} by ${book.author}`);
+function logTitleAndAuthor(book) {
+    console.log(`${book.title} by ${book.author}`);
 }
 //Write a function that logs all the books that have not been read yet, and call it.
-let books =[alice,paul,makeIt,hobit];
-function logWhoWasntRead(books){
+let books = [alice, paul, makeIt, hobit];
+function logWhoWasntRead(books) {
     var arrBook = [];
-    counter =0;
+    counter = 0;
     books.forEach(book => {
-        if (!book.beenRead){
-            console.log(book);
-            arrBook[counter]= book;
+        if (!book.beenRead) {
+            arrBook[counter] = book;
             counter++;
         }
-        
+
     });
     return arrBook;
 }
