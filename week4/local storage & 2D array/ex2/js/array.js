d923a0a5a2853a2327416c1817aecdf88e9a7c5b
Liora <lioralehrer@gmail.com>
2019-06-12 14:12:55 +0300

    array ex2

---
+++ week4/local storage & 2D array/ex2/js/array.js
@@ -0,0 +1,7 @@
+let arr10 = [[1, 2, 0, 0], [3, 4, 5, 6], [7, 0, 0, 0], [8, 9, 10, 0]];
+for (let i = 0; i < arr10.length; i++) {
+    let arr = arr10[i];
+    for (let j = 0; j<arr.length;j++){
+        console.log(arr[j]);
+    }
+}
