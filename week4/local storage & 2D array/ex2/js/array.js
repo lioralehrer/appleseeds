bb6e38271259930bf8686672f3d32626b887d152
Liora <lioralehrer@gmail.com>
2019-06-12 15:39:37 +0300

    3d array: display&sum

--- week4/local storage & 2D array/ex2/js/array.js
+++ week4/local storage & 2D array/ex2/js/array.js
@@ -1,7 +1,25 @@
-let arr10 = [[1, 2, 0, 0], [3, 4, 5, 6], [7, 0, 0, 0], [8, 9, 10, 0]];
-for (let i = 0; i < arr10.length; i++) {
-    let arr = arr10[i];
-    for (let j = 0; j<arr.length;j++){
-        console.log(arr[j]);
+let arr10 = [[1, 2], [3, 4, 5, 6], [7], [8, 9, 10]];
+function getnum(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            console.log(arr[i][j]);
+
+        }
     }
 }
+getnum(arr10);
+
+function getRowsSum(arr) {
+    let sumArr = [];
+    let sum =0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            sum += arr[i][j];                   
+        }
+       sumArr[i]=sum; 
+    }
+    console.log(sumArr);
+}
+getRowsSum(arr10);
+
+
---
+++ week5/js-contex/star-ex1-let&const/js/let-const.js
@@ -0,0 +1,10 @@
+let dog = {
+    name: "sparky"
+    };
+    let black_dog = {
+    name: "blacky"
+    }
+    const x16 = dog;
+    dog.name = "blacky";
+    x16;
+    
