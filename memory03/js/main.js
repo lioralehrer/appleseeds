f0bc74d17f1c9daf48e825fe7a27372ee71007a8
Liora <lioralehrer@gmail.com>
2019-06-15 21:59:44 +0300

    click to show specific picture

--- memory03/js/main.js
+++ memory03/js/main.js
@@ -1,18 +1,24 @@
+let game = new Game();
+var cardsArray = game.board.cards;
+
 $('.card').each(function () {
     var img = this;
     img.addEventListener('click', showPicture);
     
 });
 
-function showPicture(){
-   ("img").show(); 
+function showPicture(e){
+    let target = e.target;
+    let id = target.getAttribute("id");
+    $(`#${id} img`).show();
+    console.log(id);
 }
 
-$("img").hide();
+ $("img").hide();
 // $(".card").click((e)=>{
-//     let target = e.target;
-//     let place = target.getAttribute("place");
-//     $(place >"img").show();
+    // let target = e.target;
+    // let place = target.getAttribute("place");
+    // $(place >"img").show();
     
 // })
 
