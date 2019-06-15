2b35acf777e991253590a3db3c8ea9f61d4a41d8
Liora <lioralehrer@gmail.com>
2019-06-15 21:21:55 +0300

    entities

---
+++ memory03/js/entities/board.js
@@ -0,0 +1,17 @@
+class Board {
+    constructor(height, width) {
+        this.cards = this.createBoardOfCards(height, width);
+    }
+    createBoardOfCards(height, width) {
+        let cards = [];
+        for (let i = 0; i < height; i++) {
+            let rowOfCards = [];
+            for (let j = 0; j < width; j++) {
+                rowOfCards.push(new Card());
+
+            }
+            cards.push(rowOfCards);
+        }
+        return cards;
+    }
+}
