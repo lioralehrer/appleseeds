14df957f309c77bc88b84467c565b36dc099555d
Liora <lioralehrer@gmail.com>
2019-06-15 21:16:21 +0300

    showing cards front

--- memoryGame/js/main.js
+++ memoryGame/js/main.js
@@ -1,12 +1,13 @@
 let game = new Game();
 var cardsArray = game.board.cards;
 $(document).ready(() => {
-    let counter =1;
+    let counter = 1;
     for (let x = 0; x < 4; x++) {
-        for(let y=0;y<3;y++){
-            $(`#p${counter}`).css('background-image',`url(${cardsArray[y][x].front})`);
+        for (let y = 0; y < 3; y++) {
+            $(`#p${counter}`).css('background-image', `url(${cardsArray[y][x].front})`);
+            // $(`#p${counter}`).hide();
             counter++;
         }
-     
+
     }
 })
