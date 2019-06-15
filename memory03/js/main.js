0fe00f409704a1da1fa4b0a16f27e8ee908f2f84
Liora <lioralehrer@gmail.com>
2019-06-15 23:26:47 +0300

    game worksgit add .!

--- memory03/js/main.js
+++ memory03/js/main.js
@@ -4,22 +4,42 @@ var cardsArray = game.board.cards;
 $('.card').each(function () {
     var img = this;
     img.addEventListener('click', showPicture);
-    
-});
 
-function showPicture(e){
-    let target = e.target;
-    let id = target.getAttribute("id");
-    $(`#${id} img`).show();
-    console.log(id);
+});
+let counter = 0;
+function showPicture(e) {
+    if (counter < 2) {
+        let target = e.target;
+        let id = target.getAttribute("id");
+        let place = target.getAttribute("place");
+        $(`#${id} img`).show();
+        keepThisPlace(place);
+        counter++
+        if (counter === 2) {
+            checkmatching();
+            counter = 0;
+        }
+    }
+
+}
+let matchingArr = [];
+function keepThisPlace(place) {
+    matchingArr.push(place);
+    return matchingArr;
+}
+function checkmatching() {
+    if (matchingArr[0] === matchingArr[1]) {
+        $(`.card[place=${matchingArr[0]}]`).fadeTo("slow",0);
+       
+    }
+    else {
+        setTimeout(function(){ $("img").hide();},700);
+    }
+    matchingArr.pop();
+    matchingArr.pop();
 }
 
- $("img").hide();
-// $(".card").click((e)=>{
-    // let target = e.target;
-    // let place = target.getAttribute("place");
-    // $(place >"img").show();
-    
-// })
+$("img").hide();
+
 
 
