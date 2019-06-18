764329c647fd8f8b1cebbae4c4bd8ba4d970c748
Liora <lioralehrer@gmail.com>
2019-06-18 15:47:10 +0300

    build a function

--- week4/local storage & 2D array/geekout-ex1-transpose/js/transpose.js
+++ week4/local storage & 2D array/geekout-ex1-transpose/js/transpose.js
@@ -5,5 +5,21 @@ function transposeArry(array2D) {
     });
   
 }
-console.log(transposeArry(originArr));
+
+// 
+// build my transpose:
+function transpose(array) {
+    var newArray = [];
+    for (var i = 0; i < array.length; i++) {
+        for (var j = 0; j < array[i].length; j++) {
+            if (newArray[j] === undefined) {
+                newArray[j] = [];
+            }
+            newArray[j][i] = array[i][j];
+        }
+    }
+    return newArray;
+ }
+ console.log(transposeArry(originArr));
+ console.log(transpose(originArr));
 
