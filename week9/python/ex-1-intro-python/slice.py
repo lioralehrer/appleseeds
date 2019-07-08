12f5a0f5d5b9baaac3ac48e2a5aceddf4c96bbe3
Liora <lioralehrer@gmail.com>
2019-07-08 22:44:39 +0300

    slice

--- week9/python/ex-1-intro-python/slice.py
+++ week9/python/ex-1-intro-python/slice.py
@@ -1,2 +1,7 @@
 staff = "Hello my name is Inigo montoya, You killed my father, prepare to die"
 print(staff.split())
+staff ="Hello my name is Inigo montoya"
+print (staff[0:5])
+print (staff[6:14])
+print (staff[0::2])
+print (staff[2:15:2])
