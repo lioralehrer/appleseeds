fc7d54c64277a4c023ae6d034e4077fd3657bede
Liora <lioralehrer@gmail.com>
2019-07-09 19:56:10 +0300

    improve the fanction

--- week9/python/ex-1-intro-python/function.py
+++ week9/python/ex-1-intro-python/function.py
@@ -4,9 +4,12 @@ number = input("insert 5-digits number: ")
 def blabla(num):
     print("your number is: "+num)
     newNum = str(num)
+    sum=0
     print ("Digits in your number are: ")
     for i in range(0, 5):
       print(newNum[i])
-    print("Sum of digits:")
-    print(int(newNum[0])+int(newNum[1])+int(newNum[2])+int(newNum[3])+int(newNum[4]))
+      sum += int(newNum[i])
+    print(f"Sum of digits: {sum}")
+   
 blabla(number)
+
