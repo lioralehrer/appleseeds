b1486e347c8da22e625f92989f629d37c472d94d
Liora <lioralehrer@gmail.com>
2019-07-09 17:57:19 +0300

    index of vowels in quote

---
+++ week9/python/ex-2-speaking-python/vowels.py
@@ -0,0 +1,7 @@
+
+
+quote ="some people drink from the fountain of knowledge, others just gargle."
+vowels = "aeiou"
+index_of_vowels = [i for i, c in enumerate(quote) if c in vowels]
+print(index_of_vowels)
+    
