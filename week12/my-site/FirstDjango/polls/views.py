a68c9bd322a30fb6a1c0133803f7b5f6244e7d35
Liora <lioralehrer@gmail.com>
2019-08-03 23:21:15 +0300

    building models of Question and Choice. inset one question

--- week12/my-site/FirstDjango/polls/views.py
+++ week12/my-site/FirstDjango/polls/views.py
@@ -1,6 +1,8 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    number = random.randint(1,100)
+    return HttpResponse(f"<h1>Hello, world. You're at the polls index.</h1><h2>your number for today is:{number} </h2>")
